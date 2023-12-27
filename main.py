4f1cb29e42068de82a8af7ba726cb7738916307b
Sudhanva Narayana <nsudhanva@gmail.com>
2023-12-27 14:31:15 -0500

    langchain memory

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import (
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+)
 from langchain.chat_models import ChatOpenAI
-from langchain import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
+memory = ConversationBufferMemory(
+    memory_key="messages",
+    return_messages=True,
+)
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "messages"],
     messages=[
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}"),
     ],
 )
 
-chain = LLMChain(llm=chat, prompt=prompt)
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
 
 while True:
     content = input(">> ")
